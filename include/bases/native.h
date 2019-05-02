3400aa0fc9c8894a006b4384d92e92b439cafa0f
Filipe Medeiros <filipe@comunica.ufrn.br>
2019-05-02 11:49:13 -0300

    Criação das headers Animals Base

---
+++ include/bases/native.h
@@ -0,0 +1,27 @@
+#ifndef __NATIVE_H__
+#define __NATIVE_H__
+
+/*!
+ * @file	native.h
+ * @brief	Classe Exotic
+ * @author	Filipe Medeiros
+ * @date	02/05/2019
+ * @since	02/05/2019
+ */
+
+//! Includes and Usings
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+class Native
+{
+  protected:
+  public:
+};
+
+#endif
