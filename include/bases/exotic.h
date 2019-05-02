2df438714124f61706d3c4600d5aba54b1c6b678
Filipe Medeiros <filipe@comunica.ufrn.br>
2019-05-02 11:49:13 -0300

    Criação das headers Animals Base

---
+++ include/bases/exotic.h
@@ -0,0 +1,27 @@
+#ifndef __EXOTIC_H__
+#define __EXOTIC_H__
+
+/*!
+ * @file	exotic.h
+ * @brief	Classe Exotic
+ * @author	Filipe Medeiros
+ * @date	02/05/2019
+ * @since	02/05/2019
+ */
+
+//! Includes and Usings
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+class Exotic
+{
+  protected:
+  public:
+};
+
+#endif
---
+++ include/bases/wild.h
@@ -0,0 +1,27 @@
+#ifndef __WILD_H__
+#define __WILD_H__
+
+/*!
+ * @file	wild.h
+ * @brief	Classe Wild
+ * @author	Filipe Medeiros
+ * @date	02/05/2019
+ * @since	02/05/2019
+ */
+
+//! Includes and Usings
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+class Wild
+{
+  protected:
+  public:
+};
+
+#endif
