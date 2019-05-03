3a9c5c6cb209ad0554cc387de4bfd48d19b1c7f6
Sara Santana <saraa.beatrizz@gmail.com>
2019-05-03 10:32:36 -0300

    Classe Anfibio Remodelada

---
+++ include/animals/amphibian.h
@@ -0,0 +1,49 @@
+#ifndef __AMPHIBIAN__H_
+#define __AMPHIBIAN__H_
+
+/*!
+ *	@file		amphibian.h
+ *	@brief	Classe Anfibio
+ *	@author	Sara Santana
+ *	@date		02/05/2019
+ *	@since	02/05/2019
+ */
+
+//! Includes and Usings
+
+#include "animal.h"
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+/*!
+ *	@class	Amphibian amphibian.h
+ *	@brief	Class Amphibian
+ *	@details
+ */
+
+class Amphibian : public Animal
+{
+protected:
+  int totalSeed;
+  string lastSeed;
+
+public:
+  //! Construtor e Destrutor
+  Amphibian();
+  ~Amphibian();
+
+  //! Metodos Getters
+  int getTotalSeed();
+  string getLastSeed();
+
+  //! Metodos Setters
+  void getTotalSeed(int);
+  void getLastSeed(string);
+};
+
+#endif
