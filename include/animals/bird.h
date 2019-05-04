c3b25d5fa30cd88d3d40e11eac9bcdd269fffda6
Sara Santana <saraa.beatrizz@gmail.com>
2019-05-03 22:19:49 -0300

    Criação dos headers das classes animais

---
+++ include/animals/bird.h
@@ -0,0 +1,49 @@
+#ifndef __BIRD_H_
+#define __BIRD_H_
+
+/*!
+ *	@file	bird.h
+ *	@brief	Class Bird
+ *	@author	Sara Santana
+ *	@date	03/05/2019
+ *	@since	03/05/2019
+ */
+
+//! Includes and Usings
+
+#include "animal.h"
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+/*!
+ *	@class	Bird bird.h
+ *	@brief	Class Bird
+ *	@details
+ */
+
+class Bird: public Animal
+{
+protected:
+    double beakSize;
+    double handleSpan;
+
+public:
+  //! Construtor e Destrutor
+  Bird();
+  ~Bird();
+
+  //! Metodos Getters
+  double getBeakSize();
+  double getHandleSpan();
+
+  //! Metodos Setters
+  void setBeakSize(double);
+  void setHandleSpan(double);
+};
+
+#endif
---
+++ include/animals/reptile.h
@@ -0,0 +1,49 @@
+#ifndef __REPTILE_H_
+#define __REPTILE_H_
+
+/*!
+ *	@file	reptile.h
+ *	@brief	Class Reptile
+ *	@author	Sara Santana
+ *	@date	03/05/2019
+ *	@since	03/05/2019
+ */
+
+//! Includes and Usings
+
+#include "animal.h"
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+/*!
+ *	@class	Reptile reptile.h
+ *	@brief	Class Reptile
+ *	@details
+ */
+
+class Reptile: public Animal
+{
+protected:
+    bool isItPoisonous;
+    string poisonType;
+
+public:
+  //! Construtor e Destrutor
+  Reptile();
+  ~Reptile();
+
+  //! Metodos Getters
+  bool getIsItPoisonous();
+  string getPoisonType();
+
+  //! Metodos Setters
+  void setIsItPoisonous(bool);
+  void setPoisonType(string);
+};
+
+#endif
