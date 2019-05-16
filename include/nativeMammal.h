c8162fb0a825a8ac0e2131bf7b53dff63747f89c
Filipe Medeiros <filipe@comunica.ufrn.br>
2019-05-16 11:57:06 -0300

    Criação e Implementação do native

---
+++ include/nativeMammal.h
@@ -0,0 +1,39 @@
+#ifndef __NATIVEMAMMAL_H__
+#define __NATIVEMAMMAL_H__
+
+/*!
+ *	@file		nativeMammal.h
+ *	@brief		Classe nativeMammal
+ *	@author		Sara Santana
+ *	@date		13/05/2019
+ *	@since		13/05/2019
+ */
+
+//! Includes and Usings
+
+#include "mammal.h"
+#include "native.h"
+
+#include <string>
+using std::string;
+
+#include <iostream>
+using std::istream;
+using std::ostream;
+
+class nativeMammal : public Mammal, Native
+{
+
+public:
+    //! Construtor e Destrutor
+
+    nativeMammal();
+    nativeMammal(int, string, string, char, double, string, string, Veterinary *, Caregiver *, string, string, string);
+    ~nativeMammal();
+
+    //! Sobrecargas dos operadores
+
+    friend ostream &operator<<(ostream &, nativeMammal &);
+};
+
+#endif
---
+++ src/nativeMammal.cpp
@@ -0,0 +1,72 @@
+/*!
+ *	@file	nativeMammal.cpp
+ *	@brief	Implementação do header da classe nativeMammal
+ *	@author	Filipe Medeiros 
+ *	@date	16/05/2019
+ *	@since	16/05/2019
+ */
+
+#include "nativeMammal.h"
+
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::ostream;
+
+#include <string>
+using std::string;
+
+//!Construtor
+
+/*! @details O construtor padrão é iniciado com valores defaults */
+nativeMammal::nativeMammal()
+    : Mammal(0, "Mamifero Nativo", "", '\0', 0.0, "", "", new Veterinary, new Caregiver, ""),
+      Native("", "")
+
+{
+}
+
+/*!
+ *	@details	Este construtor recebe argumentos e atributi a classe Animal.
+ *
+ *	@param	id				Identificador do animal.
+ *	@param	animalClass		Classe do animal.
+ *	@param 	scientificName	Nome científico do animal.
+ *	@param	sex				Sexo do animal.
+ *	@param	size			Tamanho do animal.
+ *	@param	diet			Dieta do animal.
+ *	@param	baptismalName	Nome do animal.
+ *	@param	Veterinary		Veterinário do animal.
+ *	@param	Caregiver		Tratador do animal.
+ *  @param  furColor     Cor do pelo.
+ *  @param  ibamaAuth       Autorização do Ibama.
+ *  @param  UFplace      País de Origem.
+ */
+nativeMammal::nativeMammal(int id, string animalClass, string scientificName,
+                           char sex, double size, string diet, string baptismalName,
+                           Veterinary *veterinary, Caregiver *caregiver, string furColor, string ibamaAuth, string UFplace)
+
+    : Mammal(id, animalClass, scientificName, sex, size,
+             diet, baptismalName, veterinary, caregiver, furColor),
+      Native(ibamaAuth, UFplace)
+{
+}
+
+//! Destrutor
+
+/*!	@details	Destrutor padrão da classe Worker*/
+nativeMammal::~nativeMammal()
+{
+}
+
+/*!
+ * @details	Sobrecarga do operador de impressão <<
+ * @param 	os Output stream
+ * @param   Animal Classe Animal
+ */
+ostream &operator<<(ostream &os, nativeMammal &animal)
+{
+    animal.print(os);
+    animal.printWild(os);
+    return os;
+}
