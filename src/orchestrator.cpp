44d6677139af734f748914fcfae8a83090409baf
Gabriel <txagabriel@gmail.com>
2019-06-22 15:27:28 -0300

    Register animal
    Mudanças ainda podem ser feitas, tal qual <<
    Note que eu inseri direto no arquivo por que tinha uma dúvida:
    No construtor dos animais pede um apontador para veterinário e caregiver, mas supondo que você coloque 0 (0 é um valor aceito que significa que não há caregiver ou veterinário) não acho que dá certo

--- src/orchestrator.cpp
+++ src/orchestrator.cpp
@@ -11,8 +11,8 @@ void Orchestrator::menu()
     while (this->loop)
     {
         cout << "Enter a matching number:" << endl
-             << "[1] Registre employee" << endl
-             << "[2] Registre animal" << endl
+             << "[1] Register employee" << endl
+             << "[2] Register animal" << endl
              << "[3] Remove animal" << endl
              << "[4] Remove employee" << endl
              << "[5] Query an animal" << endl
@@ -28,6 +28,12 @@ void Orchestrator::menu()
         {
             loadArchives();
             addWorker();
+            break;
+        }
+        case 2:
+        {
+            loadArchives();
+            addAnimal();
             break;
         }
         case 7:
